5350176b9ad7b3b84c0a1fc2b53abb2897bbf780
ooblog <ooblog@gmail.com>
2016-03-21 02:54:01 +0900

    「onsubmit」イベントで再読み込みせずにページ移動。あと「?page=」は「github.io/」以外で動かないようにした。

--- yonmoji.js
+++ yonmoji.js
@@ -1,6 +1,6 @@
 function newhref(){
     var href=window.location.href;
-    if ( href.indexOf("?page=")>=0 ){
+    if ( href.indexOf("?page=")>=0 && href.indexOf("github.io/")>=0 ){
         var regExp=new RegExp("github\.io\/(.*)page\=","g");
         href=href.replace(regExp,"github.io/");
         href=href+".html";
@@ -17,6 +17,14 @@ else if( window.attachEvent ){
 else{
     window.onload=newhref;
 }
+
+function newword(){
+    var word=document.forms.entry.page.value;
+    var regExp=new RegExp("[/|\\|:|*|?|\"|<|>|\|]","g");
+    word=word.replace(regExp,"");
+    window.location.href=word+".html"
+}
+
 
 //# Copyright (c) 2016 ooblog
 //# License: MIT
